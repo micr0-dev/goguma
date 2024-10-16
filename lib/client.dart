f6117f19ac5d7a637e0174e0ebe3dcc17195af46
Simon Ser <contact@emersion.fr>
2024-10-16 23:47:45 +0200

    client: add ISUPPORT command

--- lib/client.dart
+++ lib/client.dart
@@ -745,6 +745,19 @@ class Client {
 		return caps;
 	}
 
+	Future<IrcIsupportRegistry> fetchIsupport() async {
+		var cmd = IrcMessage('ISUPPORT', []);
+		var batch = await _roundtripBatch(cmd, (batch) {
+			return batch.type == 'draft/isupport';
+		});
+
+		var registry = IrcIsupportRegistry();
+		for (var msg in batch.messages) {
+			registry.parse(msg.params.sublist(1, msg.params.length - 1));
+		}
+		return registry;
+	}
+
 	Future<List<ChatHistoryTarget>> fetchChatHistoryTargets(String t1, String t2) async {
 		// TODO: paging
 		var msg = IrcMessage(
