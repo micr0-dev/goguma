f80f2d3bff747978fdd8ac4571950cba26378df8
Simon Ser <contact@emersion.fr>
2025-05-10 20:45:30 +0200

    Fix react tag name in isClientTagAllowed() check

--- lib/widget/message_sheet.dart
+++ lib/widget/message_sheet.dart
@@ -83,7 +83,7 @@ class MessageSheet extends StatelessWidget {
 		// TODO: we can redact if we are channel operator too
 		var canRedact = canSendMessage && client.caps.enabled.contains('draft/message-redaction') && ircMsg.tags['msgid'] != null && isOwn && !message.entry.redacted;
 		var reactions = message.reactionMap;
-		var canReact = canSendMessage && client.caps.enabled.contains('message-tags') && client.isupport.isClientTagAllowed('react') && message.entry.networkMsgid != null;
+		var canReact = canSendMessage && client.caps.enabled.contains('message-tags') && client.isupport.isClientTagAllowed('draft/react') && message.entry.networkMsgid != null;
 
 		return Column(mainAxisSize: MainAxisSize.min, children: [
 			if (canReact) Container(
