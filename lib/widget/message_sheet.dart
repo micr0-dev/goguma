102813a224ac7af14f19f07f8e6f17bbf572278d
Calvin Lee <pounce@integraldoma.in>
2025-05-10 20:40:51 +0200

    widget/message_sheet: add support for sending reactions
    Co-authored-by: Simon Ser <contact@emersion.fr>

--- lib/widget/message_sheet.dart
+++ lib/widget/message_sheet.dart
@@ -5,9 +5,12 @@ import 'package:provider/provider.dart';
 import '../ansi.dart';
 import '../client.dart';
 import '../client_controller.dart';
+import '../database.dart';
 import '../irc.dart';
 import '../models.dart';
 import '../page/buffer.dart';
+
+const DEFAULT_REACTIONS = ['❤️', '👍', '👎', '😂', '😮', '😢'];
 
 class MessageSheet extends StatelessWidget {
 	final MessageModel message;
@@ -33,16 +36,74 @@ class MessageSheet extends StatelessWidget {
 		);
 	}
 
+	void _handleReact(BuildContext context, String reaction) async {
+		var bufferList = context.read<BufferListModel>();
+		var buffer = context.read<BufferModel>();
+		var db = context.read<DB>();
+		var client = context.read<Client>();
+		var network = context.read<NetworkModel>();
+
+		if (message.reactionMap[reaction]?.contains(client.nick) == true) {
+			ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+				content: Text('Cannot remove reaction'),
+			));
+			return;
+		}
+
+		var msg = await client.sendTextMessage(IrcMessage('TAGMSG', [buffer.name], tags: {
+			'+draft/reply': message.entry.networkMsgid!,
+			'+draft/react': reaction,
+		}));
+
+		if (client.caps.enabled.contains('echo-message')) {
+			return;
+		}
+
+		var entry = ReactionEntry(msg, buffer.id);
+		await db.storeReactions([entry]);
+		if (buffer.messageHistoryLoaded) {
+			buffer.addReactions([entry]);
+		}
+
+		bufferList.bumpLastDeliveredTime(buffer, entry.time);
+		if (network.networkEntry.bumpLastDeliveredTime(entry.time)) {
+			await db.storeNetwork(network.networkEntry);
+		}
+	}
+
 	@override
 	Widget build(BuildContext context) {
 		var ircMsg = message.msg;
 		var sender = ircMsg.source!.name;
 		var client = context.read<Client>();
+		var buffer = context.watch<BufferModel>();
+		var network = context.watch<NetworkModel>();
 		var isOwn = client.isMyNick(sender);
+		var canSendMessage = canSendMessageToBuffer(buffer, network);
 		// TODO: we can redact if we are channel operator too
-		var canRedact = client.caps.enabled.contains('draft/message-redaction') && ircMsg.tags['msgid'] != null && isOwn && !message.entry.redacted;
+		var canRedact = canSendMessage && client.caps.enabled.contains('draft/message-redaction') && ircMsg.tags['msgid'] != null && isOwn && !message.entry.redacted;
+		var reactions = message.reactionMap;
+		var canReact = canSendMessage && client.caps.enabled.contains('message-tags') && client.isupport.isClientTagAllowed('react') && message.entry.networkMsgid != null;
 
 		return Column(mainAxisSize: MainAxisSize.min, children: [
+			if (canReact) Container(
+				padding: EdgeInsets.symmetric(vertical: 10),
+					child: Row(
+					mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+					children: DEFAULT_REACTIONS.map((reaction) => IconButton.filledTonal(
+						isSelected: reactions[reaction]?.contains(client.nick) ?? false,
+						constraints: BoxConstraints(minWidth: 50, minHeight: 50),
+						onPressed: () {
+							Navigator.pop(context);
+							_handleReact(context, reaction);
+						},
+						icon: Text(
+							reaction,
+							style: TextStyle(fontSize: 20),
+						),
+					)).toList(),
+				),
+			),
 			if (onReply != null && !isOwn) ListTile(
 				title: Text('Reply'),
 				leading: Icon(Icons.reply),
